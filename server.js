a86125c4912f42ecee68a9faca82d57f3d5ac72d
Richard Hodgson <rightaboutnow@gmail.com>
2011-05-23 20:39:59 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var proton = require('proton'),
+    sys    = require('sys');
+
+proton.run(
+    require(__dirname + '/lib/webapp.js').WebApp,
+    { port: 8081 },
+    function (e) {
+        sys.error(e);
+        process.exit(1);
+    }
+);
+
