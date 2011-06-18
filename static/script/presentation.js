68d75a17d0b9216297284e811bd62f3b73427896
Richard Hodgson <rightaboutnow@gmail.com>
2011-06-19 00:58:35 +0100

    Vertically position slide contents correctly

--- static/script/presentation.js
+++ static/script/presentation.js
@@ -63,8 +63,26 @@ define(["jquery"], function ($) {
 	
 	function View () {
 		this.slides = $('.slide');
+        
+        var self = this;
+        
+        var updatePositioning = function () {
+            self.setSlideHeightFromWindow();
+            self.verticallyCentreSlideContents();
+        };
+        
+        $(window).bind("resize", updatePositioning);
+        $(window).bind("scroll", updatePositioning);
+        
+        this.wrapSlideContents();
+        updatePositioning();
 	}
 	
+    /**
+     * Returns navigation elements for all slides
+     * @param type For example, 'next' or 'previous
+     * @return jQuery 
+     */
 	View.prototype.getNavElements = function (type) {
 		if (type != 'next' && type != 'previous') {
 			throw new Error("not a known nav type");
@@ -72,9 +90,57 @@ define(["jquery"], function ($) {
 		return this.slides.find('.' + type + 'Slide');
 	}
 	
+    /**
+     * Updates the hash in the window's location
+     */
 	View.prototype.selectSlide = function (number) {
 		window.location.hash = "slide" + number;
 	}
+    
+    /**
+     * Sets the height for each slide to that of the window (viewport)
+     */
+    View.prototype.setSlideHeightFromWindow = function () {
+        this.slides.height($(window).height());
+    }
+    
+    /**
+     * Wraps contents of each slide in a div
+     * Div is used as a shortcut to work out total contents height
+     */
+    View.prototype.wrapSlideContents = function () {
+        this.slides.each(function (i) {
+            var slide = $(this),
+                paragraphs = slide.find('p:not(.navigation)');
+                
+            paragraphs.wrapAll('<div class="slideContents"></div>');
+            paragraphs.css({
+                'marginTop': 0,
+                'marginBottom': '3%'
+            });
+            
+            var contentsGroup = slide.find('.slideContents');
+            contentsGroup.css({
+                'float': 'left',
+                'width': '100%'
+            });
+        });
+    }
+    
+    /**
+     * Ensures the contents of the slide are vertically centred.
+     */
+    View.prototype.verticallyCentreSlideContents = function () {
+        this.slides.each(function (i) {
+            var slide = $(this),
+                contentsGroup = slide.find('.slideContents');
+            
+            contentsGroup.css(
+                'marginTop',
+                (slide.height() - contentsGroup.height()) / 2
+            );
+        });
+    }
 	
 	
 	function setup () {
