eed91e78a6007ff4ab7136f254a80e7598f83fba
Richard Hodgson <rightaboutnow@gmail.com>
2011-07-17 07:57:46 -0400

    Added generator tests to suite

--- tests/suite.js
+++ tests/suite.js
@@ -1,6 +1,6 @@
 var litmus = require('litmus');
 
 exports.test = new litmus.Suite('Bluff Test Suite', [
-    require('./webapp').test
-    //require('./generator').test
+    require('./webapp').test,
+    require('./generator').test
 ]);
